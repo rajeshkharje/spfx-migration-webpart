e1fd08243b79b873af9e5f8bba838802c4efd487
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:48 +0530

    Add spfx-migration/src/webparts/proxies/components/Proxies.module.scss.ts 1747413707232

---
+++ spfx-migration/src/webparts/proxies/components/Proxies.module.scss.ts
@@ -0,0 +1,37 @@
+/* tslint:disable */
+require('./Proxies.module.scss');
+
+interface IProxiesStyles {
+  header: string;
+  inputGroup: string;
+  inputField: string;
+  buttonPrimary: string;
+  messageSuccess: string;
+  messageError: string;
+  table: string;
+  deleteButton: string;
+  proxies: string;
+  container: string;
+  welcome: string;
+  description: string;
+  teams: string;
+}
+
+const styles: IProxiesStyles = {
+  proxies: 'proxies_e5b61ce4',
+  container: 'container_e5b61ce4',
+  welcome: 'welcome_e5b61ce4',
+  description: 'description_e5b61ce4',
+  teams: 'teams_e5b61ce4',
+  header: "",
+  inputGroup: "",
+  inputField: "",
+  buttonPrimary: "",
+  messageSuccess: "",
+  messageError: "",
+  table: "",
+  deleteButton: ""
+};
+
+export default styles;
+/* tslint:enable */
