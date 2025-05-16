09628fd9c34f0604712169b4b19becbee25e372e
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:21 +0530

    Add spfx-migration/src/webparts/menuPage2/components/MenuPage2.module.scss.ts 1747413800215

---
+++ spfx-migration/src/webparts/menuPage2/components/MenuPage2.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./MenuPage2.module.css");
+const styles = {
+  menuPage2: 'menuPage2_eadf022c',
+  container: 'container_eadf022c',
+  welcome: 'welcome_eadf022c',
+  description: 'description_eadf022c',
+  teams: 'teams_eadf022c'
+};
+
+export default styles;
+/* tslint:enable */
