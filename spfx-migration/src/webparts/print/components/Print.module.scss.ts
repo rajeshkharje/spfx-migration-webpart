5c96b9b3508f346d3d028b8b287fbc07b767d800
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:29 +0530

    Add spfx-migration/src/webparts/print/components/Print.module.scss.ts 1747413748499

---
+++ spfx-migration/src/webparts/print/components/Print.module.scss.ts
@@ -0,0 +1,39 @@
+/* tslint:disable */
+require('./Print.module.scss');
+
+interface IPrintStyles {
+  loadingMessage: string;
+  loadingIcon: string;
+  alertError: string;
+  alertSuccess: string;
+  title: string;
+  tabContainer: string;
+  tabContent: string;
+  label: string;
+  value: string;
+  print: string;
+  container: string;
+  welcome: string;
+  description: string;
+  teams: string;
+}
+
+const styles: IPrintStyles = {
+  print: 'print_e5b61ce4',
+  container: 'container_e5b61ce4',
+  welcome: 'welcome_e5b61ce4',
+  description: 'description_e5b61ce4',
+  teams: 'teams_e5b61ce4',
+  loadingMessage: "",
+  loadingIcon: "",
+  alertError: "",
+  alertSuccess: "",
+  title: "",
+  tabContainer: "",
+  tabContent: "",
+  label: "",
+  value: ""
+};
+
+export default styles;
+/* tslint:enable */
