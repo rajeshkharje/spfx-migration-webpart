dcacd6a5e9d789be43c987cf81659d650d340a0d
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:53 +0530

    Add spfx-migration/src/webparts/requesterWaitList/components/RequesterWaitList.module.scss.ts 1747413712353

---
+++ spfx-migration/src/webparts/requesterWaitList/components/RequesterWaitList.module.scss.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+require('./RequesterWaitList.module.scss');
+
+interface IRequesterWaitListStyles {
+  exportBtn: string;
+  tableContainer: string;
+  table: string;
+  requesterWaitList: string;
+  container: string;
+  welcome: string;
+  description: string;
+  teams: string;
+}
+
+const styles: IRequesterWaitListStyles = {
+  requesterWaitList: 'requesterWaitList_e5b61ce4',
+  container: 'container_e5b61ce4',
+  welcome: 'welcome_e5b61ce4',
+  description: 'description_e5b61ce4',
+  teams: 'teams_e5b61ce4',
+  exportBtn: "",
+  tableContainer: "",
+  table: ""
+};
+
+export default styles;
+/* tslint:enable */
