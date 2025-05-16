4d43a1abe1dee1583aeb14e9fe96a3cf920a0882
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:39 +0530

    Add spfx-migration/src/webparts/help/components/Help.module.scss.ts 1747413758740

---
+++ spfx-migration/src/webparts/help/components/Help.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./Help.module.css");
+const styles = {
+  help: 'help_40855907',
+  container: 'container_40855907',
+  welcome: 'welcome_40855907',
+  description: 'description_40855907',
+  teams: 'teams_40855907'
+};
+
+export default styles;
+/* tslint:enable */
