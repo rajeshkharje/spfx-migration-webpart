8f4a76471d69598538e3c8b46e0594d1151e5051
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:49 +0530

    Add spfx-migration/src/webparts/proxies/components/Proxies.module.scss.d.ts 1747413708563

---
+++ spfx-migration/src/webparts/proxies/components/Proxies.module.scss.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly proxies: string;
+  readonly header: string;
+  readonly inputGroup: string;
+  readonly inputLabel: string;
+  readonly inputField: string;
+  readonly buttonPrimary: string;
+  readonly messageSuccess: string;
+  readonly messageError: string;
+  readonly proxiesList: string;
+  readonly table: string;
+  readonly deleteButton: string;
+};
+export default styles;
