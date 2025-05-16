f65a1888be8c5055ceed83e65a394439aa1fc38a
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:37 +0530

    Add spfx-migration/src/webparts/print/loc/mystrings.d.ts 1747413756181

---
+++ spfx-migration/src/webparts/print/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IPrintWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'PrintWebPartStrings' {
+  const strings: IPrintWebPartStrings;
+  export = strings;
+}
