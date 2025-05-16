74d247b540c4309b0484dbfd80f7b08f4a290a38
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:16 +0530

    Add spfx-migration/src/webparts/route/components/Route.module.scss.ts 1747413735090

---
+++ spfx-migration/src/webparts/route/components/Route.module.scss.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+require('./Route.module.scss');
+
+interface IRouteStyles {
+  route: string;
+  container: string;
+  welcome: string;
+  description: string;
+  teams: string;
+}
+
+const styles: IRouteStyles = {
+  route: 'route_e5b61ce4',
+  container: 'container_e5b61ce4',
+  welcome: 'welcome_e5b61ce4',
+  description: 'description_e5b61ce4',
+  teams: 'teams_e5b61ce4'
+};
+
+export default styles;
+/* tslint:enable */
