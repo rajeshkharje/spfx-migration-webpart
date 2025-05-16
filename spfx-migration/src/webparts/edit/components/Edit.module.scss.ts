79c4873ab1389b8e5267c8456a6e1c5f4b5e611f
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:24 +0530

    Add spfx-migration/src/webparts/edit/components/Edit.module.scss.ts 1747413743790

---
+++ spfx-migration/src/webparts/edit/components/Edit.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./Edit.module.css");
+const styles = {
+  edit: 'edit_32732430',
+  container: 'container_32732430',
+  welcome: 'welcome_32732430',
+  description: 'description_32732430',
+  teams: 'teams_32732430'
+};
+
+export default styles;
+/* tslint:enable */
