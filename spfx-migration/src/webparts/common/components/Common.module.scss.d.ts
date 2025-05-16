eccec091ff855a03e08569171a0e58c5e23a4128
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:29 +0530

    Add spfx-migration/src/webparts/common/components/Common.module.scss.d.ts 1747413688495

---
+++ spfx-migration/src/webparts/common/components/Common.module.scss.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly common: string;
+  readonly container: string;
+  readonly header: string;
+  readonly section: string;
+  readonly sectionTitle: string;
+  readonly content: string;
+};
+export default styles;
