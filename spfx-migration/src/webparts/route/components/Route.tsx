30aeda2c3f72c7498b410c05057735b725251086
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:19 +0530

    Add spfx-migration/src/webparts/route/components/Route.tsx 1747413738579

---
+++ spfx-migration/src/webparts/route/components/Route.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import * as React from "react";
+import styles from "./Route.module.scss";
+
+export default function Route() {
+  return (
+    <div className={styles.route}>
+      <h2>Route Web Part</h2>
+      <p>This is the Route web part. Functionality and UI to be implemented as per requirements.</p>
+    </div>
+  );
+}
