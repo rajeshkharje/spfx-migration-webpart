3813cd87c27f37a8ca5e614d574f5cabacb8e5b4
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:28 +0530

    Add spfx-migration/src/webparts/print/components/Print.module.scss.d.ts 1747413747279

---
+++ spfx-migration/src/webparts/print/components/Print.module.scss.d.ts
@@ -0,0 +1,14 @@
+declare const styles: {
+  readonly print: string;
+  readonly loadingMessage: string;
+  readonly loadingIcon: string;
+  readonly title: string;
+  readonly tabContainer: string;
+  readonly tabContent: string;
+  readonly label: string;
+  readonly value: string;
+  readonly markRequired: string;
+  readonly alertError: string;
+  readonly alertSuccess: string;
+};
+export default styles;
