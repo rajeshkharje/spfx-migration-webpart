71322a4df98acaabccbf2f85cb127152f2b6fe28
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:10 +0530

    Add spfx-migration/src/webparts/masterList/components/MasterList.module.scss.d.ts 1747413789871

---
+++ spfx-migration/src/webparts/masterList/components/MasterList.module.scss.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+  readonly masterList: string;
+  readonly container: string;
+  readonly searchContainer: string;
+  readonly searchInput: string;
+  readonly table: string;
+  readonly filterRow: string;
+  readonly filterInput: string;
+  readonly button: string;
+};
+export default styles;
