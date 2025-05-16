2e9ac58747020e4c4cdfb1f87f696d6c49efe969
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:18 +0530

    Add spfx-migration/src/webparts/menuPage3/components/MenuPage3.module.scss.d.ts 1747413797553

---
+++ spfx-migration/src/webparts/menuPage3/components/MenuPage3.module.scss.d.ts
@@ -0,0 +1,12 @@
+declare const styles: {
+  readonly menuPage3: string;
+  readonly container: string;
+  readonly errorMessage: string;
+  readonly successMessage: string;
+  readonly formGroup: string;
+  readonly label: string;
+  readonly markRequired: string;
+  readonly input: string;
+  readonly button: string;
+};
+export default styles;
