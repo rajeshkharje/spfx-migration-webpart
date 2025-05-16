aea19d5ffaf81c04f7a952e2a484d1d8e3ce7049
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:39 +0530

    Add spfx-migration/src/webparts/edit/loc/mystrings.d.ts 1747413698426

---
+++ spfx-migration/src/webparts/edit/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IEditWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'EditWebPartStrings' {
+  const strings: IEditWebPartStrings;
+  export = strings;
+}
