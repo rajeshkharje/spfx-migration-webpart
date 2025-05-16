d4ae38afcfbcb659de1f64adabbc4fcb87fa5ec2
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:11:57 +0530

    Add spfx-migration/src/webparts/requesterWaitList/components/RequesterWaitList.module.scss.d.ts 1747413716038

---
+++ spfx-migration/src/webparts/requesterWaitList/components/RequesterWaitList.module.scss.d.ts
@@ -0,0 +1,8 @@
+declare const styles: {
+  readonly requesterWaitList: string;
+  readonly header: string;
+  readonly exportBtn: string;
+  readonly tableContainer: string;
+  readonly table: string;
+};
+export default styles;
