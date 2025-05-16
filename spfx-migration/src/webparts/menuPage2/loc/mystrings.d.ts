87640b44feb57819b65c05942ce12039c00210b8
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:03 +0530

    Add spfx-migration/src/webparts/menuPage2/loc/mystrings.d.ts 1747413782291

---
+++ spfx-migration/src/webparts/menuPage2/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IMenuPage2WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'MenuPage2WebPartStrings' {
+  const strings: IMenuPage2WebPartStrings;
+  export = strings;
+}
