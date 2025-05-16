3ca60e4ad0617df014fe71b92a23759f316efa81
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:14 +0530

    Add spfx-migration/src/webparts/masterList/components/MasterList.module.scss.ts 1747413793765

---
+++ spfx-migration/src/webparts/masterList/components/MasterList.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./MasterList.module.css");
+const styles = {
+  masterList: 'masterList_88252a77',
+  container: 'container_88252a77',
+  welcome: 'welcome_88252a77',
+  description: 'description_88252a77',
+  teams: 'teams_88252a77'
+};
+
+export default styles;
+/* tslint:enable */
