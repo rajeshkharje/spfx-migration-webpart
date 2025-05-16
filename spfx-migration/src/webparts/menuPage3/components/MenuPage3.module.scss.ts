3a6d215e7ce43bbb9872abb79c36ce8ea229d76d
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:13:20 +0530

    Add spfx-migration/src/webparts/menuPage3/components/MenuPage3.module.scss.ts 1747413798883

---
+++ spfx-migration/src/webparts/menuPage3/components/MenuPage3.module.scss.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+require("./MenuPage3.module.css");
+const styles = {
+  menuPage3: 'menuPage3_9d0c7255',
+  container: 'container_9d0c7255',
+  welcome: 'welcome_9d0c7255',
+  description: 'description_9d0c7255',
+  teams: 'teams_9d0c7255'
+};
+
+export default styles;
+/* tslint:enable */
