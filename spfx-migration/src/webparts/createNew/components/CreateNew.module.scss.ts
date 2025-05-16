58a6029f1eb6ed264bc1d1e68509adf1fb539996
rajeshkharje <40045668+rajeshkharje@users.noreply.github.com>
2025-05-16 22:12:13 +0530

    Add spfx-migration/src/webparts/createNew/components/CreateNew.module.scss.ts 1747413732532

---
+++ spfx-migration/src/webparts/createNew/components/CreateNew.module.scss.ts
@@ -0,0 +1,33 @@
+/* tslint:disable */
+require("./CreateNew.module.scss");
+const styles = {
+  createNew: 'createNew_35b2b3bf',
+  container: 'container_35b2b3bf',
+  tabNav: 'tabNav_35b2b3bf',
+  tabNavItem: 'tabNavItem_35b2b3bf',
+  tabNavItemActive: 'tabNavItemActive_35b2b3bf',
+  tabContent: 'tabContent_35b2b3bf',
+  formGroup: 'formGroup_35b2b3bf',
+  label: 'label_35b2b3bf',
+  input: 'input_35b2b3bf',
+  select: 'select_35b2b3bf',
+  textarea: 'textarea_35b2b3bf',
+  checkboxGroup: 'checkboxGroup_35b2b3bf',
+  checkboxLabel: 'checkboxLabel_35b2b3bf',
+  buttonGroup: 'buttonGroup_35b2b3bf',
+  button: 'button_35b2b3bf',
+  errorMessage: 'errorMessage_35b2b3bf',
+  contractItem: 'contractItem_35b2b3bf',
+  contractHeader: 'contractHeader_35b2b3bf',
+  contractDeleteButton: 'contractDeleteButton_35b2b3bf',
+  modalBackdrop: 'modalBackdrop_35b2b3bf',
+  modalContent: 'modalContent_35b2b3bf',
+  modalHeader: 'modalHeader_35b2b3bf',
+  modalFooter: 'modalFooter_35b2b3bf',
+  welcome: 'welcome_35b2b3bf',
+  description: 'description_35b2b3bf',
+  teams: 'teams_35b2b3bf'
+};
+
+export default styles;
+/* tslint:enable */
